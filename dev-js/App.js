38a3169a2f581f8a49d6ed9e22562b22e5ebc759
ylvayster <ylva.haggblad@gmail.com>
2017-02-25 14:00:31 +0100

    Functionality to fetch and store data from Core Reporting API with authorization of user

--- dev-js/App.js
+++ dev-js/App.js
@@ -2,21 +2,36 @@
 require('./../css/styles.scss');
 
 // global variables
-var basicConfiguration = [];
-var pagePaths = [];
+let basicConfiguration = {};
+let pagePaths = [];
+let requestStrings = [];
+//	Google Auth object
+let GoogleAuth;
+//	Retrieve the discovery document for version 3 of Google Analytics core reporting API.
+let discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/analytics/v3/rest';
+let resultData = [];
+let callId = 0;	//	init
+//	Access scope for API calls; read only
+const SCOPE = ['https://www.googleapis.com/auth/analytics.readonly'];
+//	client ID for app
+const CLIENT_ID = '540679893971-kjh5477188leiq81h80vuq3s415n0ren.apps.googleusercontent.com';
 
-//	fetch DOM nodes
+//	DOM nodes
 let addTableRowBtn = document.getElementById("add");
 let calculateButton = document.getElementById("calculate");
 let pageConfigurationForm = document.getElementById("pageConfigurationForm");
 let basicConfigurationForm = document.getElementById("basicConfigurationForm");
 let configurationTable = document.getElementById("pageConfigurationTable");
 let configurationTableSection = document.getElementById("pagesToTest");
+let authButton = document.getElementById("auth-button");
+let resultTable = document.getElementById("resultTable");
 
-//	add event listeners
-addTableRowBtn.addEventListener("click", appendTableRow);
+//	Event listeners
+addTableRowBtn.addEventListener("click", appendConfTableRow);
 addTableRowBtn.addEventListener("click", pushPagePath);
 calculateButton.addEventListener("click", addBasicConfiguration);
+calculateButton.addEventListener("click", createRequestStrings);
+authButton.addEventListener('click', authorize);
 
 // Function to enable/disable button
 function toggleDisableButton (buttonId, shouldDisable) {
@@ -41,11 +56,12 @@ function addBasicConfiguration (evt, formInput) {
 	for(let i = 0; i < inputElements.length; i++){
 		Object.assign(newBasicConf, { [inputElements[i].name] : inputElements[i].value});
 	}
+	basicConfiguration = newBasicConf;
 }
 
 //	Function for appending page configuration to table by #add-button
 //	todo: ADD DELETE BUTTON FOR EACH ROW
-function appendTableRow(evt, tableInput) {
+function appendConfTableRow(evt, tableInput) {
 	evt.preventDefault();
 	let inputElements = pageConfigurationForm.elements;
 	let tr = document.createElement("tr");
@@ -66,14 +82,116 @@ function appendTableRow(evt, tableInput) {
 
 //	Function for adding page configuration to page conf-array
 function pushPagePath(evt, tableInput){
-evt.preventDefault();
+evt.preventDefault(); 
 let inputElements = pageConfigurationForm.elements;
 let newPagePath = {
+	callId : callId,
 	pagePath : inputElements[0].value,
-	engagementGoal : inputElements[1].value,
+	actionGoal : inputElements[1].value,
 	conversionGoal : inputElements[2].value
 };
+	//	add new object to resultData array for future storage of result
+	resultData.splice(callId, 0, {});
+	callId++;
+	//	push newPagePath to pagePaths array
 	pagePaths.push(newPagePath);
+}
+
+//	Function for creating API request string. Arguments - single object from pagePaths array
+function createRequestStrings (event) {
+	let engagementGoal;
+	let actionGoal; 
+	let conversionGoal;
+	//	empty requestStrings array to avoid multiple calls
+	//	solution as suggested in http://stackoverflow.com/a/1232046
+	requestStrings.length = 0;
+
+	pagePaths.forEach(function(element){
+		engagementGoal = { 'callId' : element.callId, 'type' : 'engagementGoal',  'pagePath' : element.pagePath, 'metrics': 'ga:users,ga:bounceRate', 'segment' : `users::sequence::ga:pagePath=${element.pagePath}` };
+		actionGoal = { 'callId' : element.callId, 'type' : 'actionGoal', 'pagePath' : element.pagePath, 'metrics': 'ga:users', 'segment' : `users::sequence::ga:pagePath=${element.pagePath}->>${element.actionGoal}` };
+		conversionGoal = { 'callId' : element.callId, 'type' : 'conversionGoal', 'pagePath' : element.pagePath, 'metrics': 'ga:users', 'segment' : `users::sequence::ga:pagePath=${element.pagePath}->>${element.actionGoal}->>${element.conversionGoal}`};
+		requestStrings.push(engagementGoal, actionGoal, conversionGoal);
+		//	console.log('requestStrings', requestStrings);		
+	});
+
+	// authorize users and start communication with API when request strings are created
+	authorize(event);
+}
+
+// Initialize the gapi.client object, which app uses to make API requests.
+function authorize(event) {
+// Handles the authorization flow.
+// `immediate` should be false when invoked from the button click.
+gapi.client.load('analytics', 'v3');
+let useImmediate = event ? false : true;
+let authData = {
+  client_id: CLIENT_ID,
+  scope: SCOPE,
+  immediate: useImmediate
+};
+
+gapi.auth.authorize(authData, function(response) {
+//	if user denies the permission of the app to read Google Analytics data, display 'Authorize'-button so that user can authorize later on if wished
+  if (response.error) {
+    authButton.hidden = false;
+  }
+  else {
+    authButton.hidden = true;
+    makeAPICall();
+  	}
+});
+}
+
+//	Function for sending request to Core reporting API when user is authourized
+function makeAPICall () {
+		requestStrings.forEach(function(element){
+		 	//	delay of 0.1s per API call to avoid reaching call limit per second	
+		window.setTimeout(function(){
+			queryCoreReportingApi(element);
+		}, 100)
+		});
+	}
+
+//	Function for querying the Core reporting API, saves data for each response
+function queryCoreReportingApi(element) {
+ gapi.client.analytics.data.ga.get({
+    'ids': 'ga:' + basicConfiguration.gaViewId,		//	  
+    'start-date': `${basicConfiguration.samplePeriod * 7}daysAgo`,  //	user input is in weeks
+    'end-date': 'yesterday',
+    'samplingLevel' : basicConfiguration.samplingLevel,
+    'metrics': element.metrics,
+    'segment' : element.segment,
+    'max-results' : 10000 //	https://developers.google.com/analytics/devguides/reporting/core/v3/reference#maxResults 
+
+  })
+  .then(function(response) {
+    var formattedJson = JSON.stringify(response.result, null, 2);
+    console.log(formattedJson);
+    saveResult(response.result, element.callId, element.type, element.pagePath);
+  })
+  .then(null, function(err) {
+      // Log any errors
+      console.log(err);
+  });
+}
+
+//	Function for saving relevant data for calculation from API-response
+function saveResult(result, callId, type, pagePath){
+	let resultObject = resultData[callId];
+
+	if(type === "engagementGoal"){
+		let engagementGoalUsers = result["totalsForAllResults"]["ga:users"];
+		let bounceRate = result["totalsForAllResults"]["ga:bounceRate"]
+		Object.assign(resultObject, { 'pagePath': pagePath, 'engagementGoalUsers' : engagementGoalUsers, 'bounceRate' : bounceRate});
+	} else if(type === "actionGoal") {
+		let actionGoalUsers = result["totalsForAllResults"]["ga:users"];
+		Object.assign(resultObject, { 'actionGoalUsers' : actionGoalUsers });
+	} else if(type === "conversionGoal") {	
+		let conversionGoalUsers = result["totalsForAllResults"]["ga:users"];
+		Object.assign(resultObject, { 'conversionGoalUsers' : conversionGoalUsers });
+	} else {
+		console.log("Unknown type");
+	}
 }
 
 // 	Function to validate input
@@ -91,14 +209,3 @@ let newPagePath = {
 //	Function for saving data in local storage
 
 //	?? Function to regEx input ?? 
-
-//	Function for creating API request string
-
-//	Function for sending request to Core reporting API via #calculate-button
-	//	delay of 0.1s per API call to avoid reaching call limit per second
-
-//	Function for handling API-response
-
-//	Function for calculating result
-
-//	Function for displaying result
