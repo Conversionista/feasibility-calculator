07c344702635071ccd2743f442b6911e6407d470
ylvayster <ylva.haggblad@gmail.com>
2017-03-01 10:18:22 +0100

    Refactoring of code. Storage & display of calculated data

--- dev-js/App.js
+++ dev-js/App.js
@@ -19,21 +19,44 @@ const CLIENT_ID = '540679893971-kjh5477188leiq81h80vuq3s415n0ren.apps.googleuser
 
 //	DOM nodes
 let addTableRowBtn = document.getElementById("add");
-let calculateButton = document.getElementById("calculate");
+let calculateBtn = document.getElementById("calculate");
 let pageConfigurationForm = document.getElementById("pageConfigurationForm");
 let basicConfigurationForm = document.getElementById("basicConfigurationForm");
 let configurationTable = document.getElementById("pageConfigurationTable");
 let configurationTableSection = document.getElementById("pagesToTest");
-let authButton = document.getElementById("auth-button");
+let authBtn = document.getElementById("authButton");
 let resultTable = document.getElementById("resultTable");
+let resultSection = document.getElementById("result");
 
 //	Event listeners
-addTableRowBtn.addEventListener("click", appendConfTableRow);
-addTableRowBtn.addEventListener("click", pushPagePath);
-calculateButton.addEventListener("click", addBasicConfiguration);
-calculateButton.addEventListener("click", createRequestStrings);
-calculateButton.addEventListener("click", authorize);
-authButton.addEventListener('click', authorize);
+addTableRowBtn.addEventListener("click", addButton);
+calculateBtn.addEventListener("click", calculateButton);
+authBtn.addEventListener("click", authorize);
+
+//	function to add behaviour to add-button
+function addButton(evt){
+	evt.preventDefault();
+	toggleDisableButton("calculate", true);
+	addBasicConfiguration();
+	pushPagePath();
+	createRequestStrings();
+	authorize();
+	
+	//	timeout function to delay possibility for user to calculate result on data before it has been fetched from the API
+	window.setTimeout(function(){
+		appendConfTableRow();
+		toggleDisableButton("calculate", false);		
+	}, 2000);
+}
+
+//	function to add behavior to calculate-button
+function calculateButton(evt){
+	evt.preventDefault();
+	resultData.forEach(function(element){
+		calculateResult(element);
+	 });
+	displayResult();
+}
 
 // Function to enable/disable button
 function toggleDisableButton (buttonId, shouldDisable) {
@@ -51,8 +74,7 @@ function toggleCssClass(element, className){
 }
 
 //	Function for adding basic configuratíon to conf-array
-function addBasicConfiguration (evt, formInput) {
-	evt.preventDefault();
+function addBasicConfiguration () {
 	let inputElements = basicConfigurationForm.elements;
 	let newBasicConf = {};
 	for(let i = 0; i < inputElements.length; i++){
@@ -63,8 +85,7 @@ function addBasicConfiguration (evt, formInput) {
 
 //	Function for appending page configuration to table by #add-button
 //	todo: ADD DELETE BUTTON FOR EACH ROW
-function appendConfTableRow(evt, tableInput) {
-	evt.preventDefault();
+function appendConfTableRow() {
 	let inputElements = pageConfigurationForm.elements;
 	let tr = document.createElement("tr");
 	
@@ -83,20 +104,19 @@ function appendConfTableRow(evt, tableInput) {
 }
 
 //	Function for adding page configuration to page conf-array
-function pushPagePath(evt, tableInput){
-evt.preventDefault(); 
-let inputElements = pageConfigurationForm.elements;
-let newPagePath = {
-	rowId : rowId,
-	pagePath : inputElements[0].value,
-	actionGoal : inputElements[1].value,
-	conversionGoal : inputElements[2].value
-};
-	//	add new object to resultData array for future storage of result
-	resultData.splice(rowId, 0, {});
-	rowId++;
-	//	push newPagePath to pagePaths array
-	pagePaths.push(newPagePath);
+function pushPagePath(){
+	let inputElements = pageConfigurationForm.elements;
+	let newPagePath = {
+		rowId : rowId,
+		pagePath : inputElements[0].value,
+		actionGoal : inputElements[1].value,
+		conversionGoal : inputElements[2].value
+	};
+		//	add new object to resultData array for future storage of result
+		resultData.splice(rowId, 0, {});
+		rowId++;
+		//	push newPagePath to pagePaths array
+		pagePaths.push(newPagePath);
 }
 
 // Initializes the gapi.client object, which app uses to make API requests.
@@ -114,10 +134,10 @@ function authorize(event) {
 		gapi.auth.authorize(authData, function(response) {
 		//	if user denies the permission of the app to read Google Analytics data, display 'Authorize'-button so that user can authorize later on if wished
 		  if (response.error) {
-		    authButton.hidden = false;
+		    authBtn.hidden = false;
 		  }
 		  else {
-		    authButton.hidden = true;
+		    authBtn.hidden = true;
 		    makeAPICall();
 		  	}
 		});
@@ -125,7 +145,7 @@ function authorize(event) {
 }
 
 //	Function for creating API request string. Arguments - single object from pagePaths array
-function createRequestStrings (event) {
+function createRequestStrings() {
 	let engagementGoal;
 	let actionGoal; 
 	let conversionGoal;
@@ -136,15 +156,15 @@ function createRequestStrings (event) {
 	
 	pagePaths.forEach(function(element){
 		engagementGoal = { 'rowId' : element.rowId, 'type' : 'engagementGoal',  'pagePath' : element.pagePath, 'metrics': 'ga:users,ga:bounceRate', 'segment' : `users::sequence::ga:pagePath=${element.pagePath}` };
-		actionGoal = { 'rowId' : element.rowId, 'type' : 'actionGoal', 'pagePath' : element.pagePath, 'metrics': 'ga:users', 'segment' : `users::sequence::ga:pagePath=${element.pagePath};->>${element.actionGoal}` };
-		conversionGoal = { 'rowId' : element.rowId, 'type' : 'conversionGoal', 'pagePath' : element.pagePath, 'metrics': 'ga:users', 'segment' : `users::sequence::ga:pagePath=${element.pagePath};->>${element.actionGoal};->>${element.conversionGoal}`};
-		requestStrings.push(engagementGoal, actionGoal, conversionGoal);
+		actionGoal = { 'rowId' : element.rowId, 'type' : 'actionGoal', 'pagePath' : element.pagePath, 'actionGoal' : element.actionGoal, 'metrics': 'ga:users', 'segment' : `users::sequence::ga:pagePath=${element.pagePath};->>${element.actionGoal}` };
+		conversionGoal = { 'rowId' : element.rowId, 'type' : 'conversionGoal', 'pagePath' : element.pagePath, 'conversionGoal' : element.conversionGoal, 'metrics': 'ga:users', 'segment' : `users::sequence::ga:pagePath=${element.pagePath};->>${element.actionGoal};->>${element.conversionGoal}`};
+		requestStrings.push([engagementGoal, actionGoal, conversionGoal]);
 	});
 }
 
 //	Function for sending request to Core reporting API when user is authourized
 function makeAPICall () {
-		requestStrings.forEach(function(element){
+	requestStrings[requestStrings.length - 1].forEach(function(element){
 			queryCoreReportingApi(element);
 	});
 }
@@ -161,13 +181,8 @@ function queryCoreReportingApi(element) {
     'max-results' : 10000 //	https://developers.google.com/analytics/devguides/reporting/core/v3/reference#maxResults 
   })
   .then(function(response) {
-    saveResult(response.result, element.rowId, element.type, element.pagePath);
+    saveResult(response.result, element.rowId, element.type, element.pagePath, element.actionGoal, element.conversionGoal);
   })
-  .then(function() {
-	resultData.forEach( function(resultElement){
-		calculateResult(resultElement);
-	});
-   })
   .then(null, function(err) {
       // Log any errors
       console.log(err);
@@ -175,77 +190,82 @@ function queryCoreReportingApi(element) {
 }
 
 //	Function for saving relevant data for calculation from API-response
-function saveResult(result, rowId, type, pagePath){
+function saveResult(result, rowId, type, pagePath, actionGoal, conversionGoal){
 	let resultObject = resultData[rowId];
-	Object.assign(resultObject, {"type" : type, "rowId": rowId});
+	Object.assign(resultObject, {rowId});
 
 	if(type === "engagementGoal"){
-		let engagementGoalUsers = result["totalsForAllResults"]["ga:users"];
-		let bounceRate = result["totalsForAllResults"]["ga:bounceRate"];
-		Object.assign(resultObject, { 'pagePath': pagePath, 'bounceRate' : bounceRate, 'engagementGoalUsers' : engagementGoalUsers});
+		let engagementGoalUsers = result.totalsForAllResults["ga:users"];
+		let bounceRate = result.totalsForAllResults["ga:bounceRate"];
+		Object.assign(resultObject, { pagePath, bounceRate, engagementGoalUsers});
 	} else if(type === "actionGoal") {
-		let actionGoalUsers = result["totalsForAllResults"]["ga:users"];
-		Object.assign(resultObject, { 'actionGoalUsers' : actionGoalUsers });
+		let actionGoalUsers = result.totalsForAllResults["ga:users"];
+		Object.assign(resultObject, { actionGoal, actionGoalUsers });
 	} else if(type === "conversionGoal") {	
-		let conversionGoalUsers = result["totalsForAllResults"]["ga:users"];
-		Object.assign(resultObject, { 'conversionGoalUsers' : conversionGoalUsers });
+		let conversionGoalUsers = result.totalsForAllResults["ga:users"];
+		Object.assign(resultObject, { conversionGoal, conversionGoalUsers });
 	} else {
 		console.log("Unknown type");
 	}
-	console.log(resultData);
 }
 
 //	Function for calculating result
 function calculateResult(element){
-	let bounceRate = element.bounceRate;
+	let bounceRate = Number(element.bounceRate);
 	let engagementGoalUsers = Number(element.engagementGoalUsers);
 	let noOfVariations = basicConfiguration.variations;
 	let maxExperimentLength = basicConfiguration.experimentLength; 
 	let samplePeriod = basicConfiguration.samplePeriod;
 	let type = element.type;
+	let rowId = element.rowId;
+	let engagementRateShare = (bounceRate !== 0 ? (1 - (bounceRate/100)) : 1);
+	let actionGoalUsers = Number(element.actionGoalUsers);
+	let conversionGoalUsers = Number(element.conversionGoalUsers);
+	let engagementGoalMDU;
+	let actionCTR;
+	let actionGoalMDU;
+	let CR;
+	let conversionGoalMDU;
 	
-	if(type === "engagementGoal"){
-		let engagementGoalMDU;
-		let engagementRateShare = (bounceRate !== 0 ? (1 - (bounceRate/100)) : 1);
-		
-		if(engagementGoalUsers !== 0){
-			engagementGoalMDU = engagementGoalMinDetectUplift(noOfVariations, engagementRateShare, engagementGoalUsers, samplePeriod, maxExperimentLength);	
-		}else{
-			engagementGoalMDU = ("N/A");
-		}
-		console.log('emdu', engagementGoalMDU);
-	
-	} else if(type === "actionGoal") {
-		let actionCTR;
-		let actionGoalMDU;
-		let actionGoalUsers = Number(element.actionGoalUsers);
-		
-		if(actionGoalUsers && engagementGoalUsers !== 0 ){
-			actionCTR = actionGoalClickThroughRate(actionGoalUsers, engagementGoalUsers);
-			actionGoalMDU = actionGoalMinDetectUplift(noOfVariations, actionCTR, engagementGoalUsers, samplePeriod, maxExperimentLength);
-		} else {
-			actionCTR = ("N/A");
-			actionGoalMDU = ("N/A");
-		}
-		console.log('actr, acmdu', actionCTR, actionGoalMDU);
-	
-	} else if(type === "conversionGoal") {
-		let CR;
-		let conversionGoalMDU;
-		let conversionGoalUsers = Number(element.conversionGoalUsers);
-		
-		if(conversionGoalUsers !== 0 && engagementGoalUsers !== 0){
-			CR = conversionRate(conversionGoalUsers, engagementGoalUsers);
-			conversionGoalMDU = conversionGoalMinDetectUplift(noOfVariations, CR, engagementGoalUsers, samplePeriod, maxExperimentLength);	
-		}else{
-			CR = ("N/A");
-			conversionGoalMDU = ("N/A");
-		}
-		console.log('cr, crmdu', CR, conversionGoalMDU);
-	
-	} else {
-		console.log("Unknown type");
+	//	add new object to calculatedResult array for future storage of result if no object on rowId-position is available
+	if (!calculatedResult[rowId]){
+		calculatedResult.splice(rowId, 0, {});
 	}
+	
+	if (engagementGoalUsers !== 0){
+		engagementGoalMDU = engagementGoalMinDetectUplift(noOfVariations, engagementRateShare, engagementGoalUsers, samplePeriod, maxExperimentLength);	
+	} else {
+		engagementGoalMDU = ("N/A");
+	}
+	
+	if (actionGoalUsers && engagementGoalUsers !== 0 ){
+		actionCTR = actionGoalClickThroughRate(actionGoalUsers, engagementGoalUsers);
+		actionGoalMDU = actionGoalMinDetectUplift(noOfVariations, actionCTR, engagementGoalUsers, samplePeriod, maxExperimentLength);
+	} else {
+		actionCTR = ("N/A");
+		actionGoalMDU = ("N/A");
+	}
+		
+	if (conversionGoalUsers && engagementGoalUsers !== 0){
+		CR = conversionRate(conversionGoalUsers, engagementGoalUsers);
+		conversionGoalMDU = conversionGoalMinDetectUplift(noOfVariations, CR, engagementGoalUsers, samplePeriod, maxExperimentLength);	
+	} else {
+		CR = ("N/A");
+		conversionGoalMDU = ("N/A");
+	}
+
+	saveCalculatedResult(element.pagePath, element.actionGoal, element.conversionGoal, rowId, {
+		bounceRate,
+		engagementGoalMDU,
+		engagementGoalUsers,
+		engagementRateShare,
+		actionGoalUsers,
+		actionCTR,
+		actionGoalMDU,
+		conversionGoalUsers,
+		CR, 
+		conversionGoalMDU
+	});
 }
 
 function engagementGoalMinDetectUplift(noOfVariations, engagementRateShare, engagementGoalUsers, samplePeriod, maxExperimentLength){
@@ -254,23 +274,116 @@ function engagementGoalMinDetectUplift(noOfVariations, engagementRateShare, enga
 }
 	
 function actionGoalClickThroughRate(actionGoalUsers, engagementGoalUsers){
-	let actionGoalClickThroughRate = (actionGoalUsers/engagementGoalUsers);
+	let actionGoalClickThroughRate = 100 *(actionGoalUsers/engagementGoalUsers);
 	return actionGoalClickThroughRate;
 }
 
 function actionGoalMinDetectUplift(noOfVariations, actionGoalClickThroughRate, engagementGoalUsers, samplePeriod, maxExperimentLength){
-	let actionGoalMinDetectUplift = 100 * Math.sqrt(26*noOfVariations*(1-actionGoalClickThroughRate)/actionGoalClickThroughRate/((engagementGoalUsers/samplePeriod)*maxExperimentLength));
+	let actionGoalMinDetectUplift = 100 * Math.sqrt(26*noOfVariations*(1-(actionGoalClickThroughRate/100))/(actionGoalClickThroughRate/100)/((engagementGoalUsers/samplePeriod)*maxExperimentLength));
 	return actionGoalMinDetectUplift;
 }
 
 function conversionRate(conversionGoalUsers, engagementGoalUsers){
-	let conversionRate = (conversionGoalUsers/engagementGoalUsers);
+	let conversionRate = 100 * (conversionGoalUsers/engagementGoalUsers);
 	return conversionRate;
 }
 
 function conversionGoalMinDetectUplift(noOfVariations, conversionRate, engagementGoalUsers, samplePeriod, maxExperimentLength){
-	let conversionGoalMinDetectUplift = 100 * Math.sqrt(26*noOfVariations*(1-conversionRate)/conversionRate/((engagementGoalUsers/samplePeriod)*maxExperimentLength));
+	let conversionGoalMinDetectUplift = 100 * Math.sqrt(26*noOfVariations*(1-(conversionRate/100))/(conversionRate/100)/((engagementGoalUsers/samplePeriod)*maxExperimentLength));
 	return conversionGoalMinDetectUplift;
+}
+
+function saveCalculatedResult(pagePath, actionGoal, conversionGoal, rowId, calculatedValues){
+	let resultObject = calculatedResult[rowId];
+	let bounceRate = calculatedValues.bounceRate;
+	let engagementGoalUsers = calculatedValues.engagementGoalUsers;
+	let engagementGoalMDU = calculatedValues.engagementGoalMDU;
+	let actionGoalUsers = calculatedValues.actionGoalUsers ;
+	let actionCTR = calculatedValues.actionCTR;
+	let actionGoalMDU = calculatedValues.actionGoalMDU;
+	let conversionGoalUsers = calculatedValues.conversionGoalUsers;
+	let conversionRate = calculatedValues.CR;
+	let conversionGoalMinDetectUplift = calculatedValues.conversionGoalMDU;
+	
+	Object.assign(resultObject, { 
+		pagePath,
+		actionGoal,
+		conversionGoal,
+		rowId,
+		bounceRate,
+		engagementGoalUsers,
+		engagementGoalMDU,
+		actionGoalUsers,
+		actionCTR,
+		actionGoalMDU,
+		conversionGoalUsers,
+		conversionRate,
+		conversionGoalMinDetectUplift
+	});
+}
+
+function displayResult(){
+	calculatedResult.forEach(appendResultTableRow);
+	resultSection.classList.remove("hidden");
+}
+
+
+function appendResultTableRow(element){
+	let tr = document.createElement("tr");
+	tr.setAttribute("id", `tr${element.rowId}`);
+
+	if (!document.getElementById(`tr${element.rowId}`)) {
+		for(let i = 0; i < 12; i++){
+		var td = document.createElement("td");
+		td.setAttribute("id", `td${i}`);
+
+		switch (i) {
+		  case 0:
+		    td.innerHTML = element.pagePath;
+		    break;
+		  case 1:
+		  //	formatting of thousands eg 17000 --> 17 000
+		  //	see http://stackoverflow.com/a/2901298
+		    td.innerHTML = element.engagementGoalUsers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+		    break;
+		  case 2:
+		    td.innerHTML = `${element.bounceRate.toFixed(1)}%`;
+		    break;
+		  case 3:
+		    td.innerHTML = `${element.engagementGoalMDU.toFixed(1)}%`;
+		    break;
+		  case 4:
+		    td.innerHTML = element.actionGoal;
+		    break;
+		  case 5:
+		  	td.innerHTML = element.actionGoalUsers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+		    break;
+		  case 6:
+		  	td.innerHTML = `${element.actionCTR.toFixed(1)}%`;
+		    break;
+		  case 7:
+		  	td.innerHTML = `${element.actionGoalMDU.toFixed(1)}%`;
+		    break;
+		  case 8:
+		  	td.innerHTML = element.conversionGoal;
+		    break;
+		  case 9:
+		  	td.innerHTML = element.conversionGoalUsers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+		    break;
+		  case 10:
+		  	td.innerHTML = `${element.conversionRate.toFixed(1)}%`;
+		    break;
+		  case 11:
+		  	td.innerHTML = `${element.conversionGoalMinDetectUplift.toFixed(1)}%`;
+		    break;
+		  default:
+		    td.innerHTML = "nothing yet";
+		}
+
+		tr.appendChild(td);
+		resultTable.appendChild(tr);
+		}
+	}
 }
 
 //	Function for saving calculated result
